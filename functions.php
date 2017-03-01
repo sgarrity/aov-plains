5877e5f5e56615e7ff1417396cc7b4e71afd68b4
Steven Garrity <steven@silverorange.com>
2017-03-01 14:55:31 +0000

    Remove unused javascript/css

--- functions.php
+++ functions.php
@@ -141,3 +141,6 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
