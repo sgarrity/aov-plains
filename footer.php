2936e658a3831cb4a6a85ad3db4a71a2920a3355
Steven Garrity <steven@silverorange.com>
2017-03-01 14:54:53 +0000

    Remove footer junk

--- footer.php
+++ footer.php
@@ -15,9 +15,6 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'plains' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'plains' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'plains' ), 'plains', '<a href="https://automattic.com/" rel="designer">Steven Garrity</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
